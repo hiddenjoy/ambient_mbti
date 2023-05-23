d9fa4a79ee35b209ed4f99a27542046c63caaa29
hiddenjoy <hiddenjoy@snu.ac.kr>
2023-05-23 13:51:45 +0900

    update: index.js

---
+++ src/pages/compare.js
@@ -0,0 +1,9 @@
+export default function Compare() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-6xl font-bold text-center text-tertiary">
+        MBTI vs. MBTI
+      </h1>
+    </main>
+  );
+}
---
+++ src/pages/my_page.js
@@ -0,0 +1,7 @@
+export default function Mypage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-6xl font-bold text-center text-tertiary">Mypage</h1>
+    </main>
+  );
+}
