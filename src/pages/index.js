962b98cba92073c2d2c1df04b5ce63c7f08efcf0
shsha0110 <shsh0201@snu.ac.kr>
2023-05-27 16:22:59 +0900

    update: sign in page로 redirection
    홈에 로그인 되지 않은 경우, 로그인 페이지로 리디렉션 하는 코드 추가

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,16 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
+  const router = useRouter();
+  const { data } = useSession({
+    required : true,
+    onUnauthenticated() {
+      router.replace("/auth/signin");
+    },
+  });
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-6xl font-bold text-center text-primary">
