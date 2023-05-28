00fa084dcee95581e7c12d9f5e3424f9b15f528c
shsha0110 <shsh0201@snu.ac.kr>
2023-05-28 22:27:19 +0900

    create: administrator data
    관리자 데이터 생성. 이름과 MBTI

---
+++ src/data/admins.js
@@ -0,0 +1,27 @@
+export const admins = [
+    {
+      name: "권혁범",
+      mbti: "INTP"
+    },
+
+    {
+        name: "박은빈",
+        mbti: "ENFP"
+    },
+
+    {
+        name: "유성현",
+        mbti: "INFJ"
+    },
+
+    {
+        name: "이지예",
+        mbti: "ESFJ"
+    },
+
+    {
+        name: "장다예",
+        mbti: "ENTP"
+    },
+  ];
+  
