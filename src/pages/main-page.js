bea2d39386ab8cf122b9e471db10d48bacabbe7f
hiddenjoy <hiddenjoy@snu.ac.kr>
2023-05-28 23:15:28 +0900

    smallanswer 컴포넌트 만들고 answer 더미데이터 만들어봄

--- src/pages/main-page.js
+++ src/pages/main-page.js
@@ -4,6 +4,9 @@ import { useSession } from "next-auth/react";
 import { useState, useEffect } from "react";
 import { doc, getDoc } from "firebase/firestore";
 import { db } from "@/firebase/index.js";
+import Question from "../components/Question";
+import SmallAnswer from "../components/SmallAnswer";
+import { answer } from "@/data/answer.js";
 
 const Main = () => {
   const { data: session } = useSession();
@@ -28,25 +31,7 @@ const Main = () => {
     <>
       {console.log(user)}
       <main className="flex min-h-screen flex-col items-center divide-y divide-slate-700">
-        {/* 질문 */}
-        <div className="bg-neutral-300 w-full flex flex-col items-center p-5 text-black">
-          <div className="w-3xl p-5 bg-lime-100 rounded-3xl">
-            <h1 className="text-xl font-bold mb-3">오늘의 질문</h1>
-            <p className="text-center">
-              <b className="underline">{user?.name}</b>님이 가장 '나'다워지는
-              순간은 언제인가요?
-            </p>
-            <div className="flex ">
-              <input
-                className="w-full p-2 mt-3 border-2 border-neutral-400 rounded-lg"
-                placeholder="답변을 입력해주세요"
-              ></input>
-              <button type="submit" className="w-1/5 p-2 mt-3 border-2">
-                제출
-              </button>
-            </div>
-          </div>
-        </div>
+        <Question />
         {/* 랜덤한 답변 보여주기 */}
         <div className="bg-white w-full p-5 text-black">
           <h1>{user?.mbti} 친구들의 답변 모아보기</h1>
@@ -62,22 +47,9 @@ const Main = () => {
         <div className=" bg-neutral-200 w-full p-5 text-black">
           MBTI별 인기 답변
           <div className="mt-3 grid grid-cols-4 gap-4">
-            <div className="bg-lime-100">01</div>
-            <div className="bg-lime-100">02</div>
-            <div className="bg-lime-100">03</div>
-            <div className="bg-lime-100">04</div>
-            <div className="bg-lime-100">05</div>
-            <div className="bg-lime-100">06</div>
-            <div className="bg-lime-100">07</div>
-            <div className="bg-lime-100">08</div>
-            <div className="bg-lime-100">09</div>
-            <div className="bg-lime-100">10</div>
-            <div className="bg-lime-100">11</div>
-            <div className="bg-lime-100">12</div>
-            <div className="bg-lime-100">13</div>
-            <div className="bg-lime-100">14</div>
-            <div className="bg-lime-100">15</div>
-            <div className="bg-lime-100">16</div>
+            {answer.map((item) => (
+              <SmallAnswer answer={item} />
+            ))}
           </div>
         </div>
         <Link
