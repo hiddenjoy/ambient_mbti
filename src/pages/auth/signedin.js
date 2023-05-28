77ea16e64f38faa6b61d19c0d8ca55e3bf4c0309
shsha0110 <shsh0201@snu.ac.kr>
2023-05-28 15:50:09 +0900

    create: signedin.js
    signin.js에서 signed-in된 사용자에게 Signed-in 되었다는 정보를 제공하던 방식에서, signedin 페이지를 새롭게 렌더링하는 방식으로 변경.

---
+++ src/pages/auth/signedin.js
@@ -0,0 +1,75 @@
+import { useRouter } from "next/router";
+import { useSession, signIn, signOut } from "next-auth/react";
+import { useState, useEffect } from "react";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from '@/firebase/index.js';
+
+export default function SignedIn() {
+  const router = useRouter();
+  const { data: session } = useSession();
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    async function fetchUser() {
+      if (session) {
+        const userRef = doc(db, 'users', session.user.id);
+        const userDoc = await getDoc(userRef);
+
+        if (userDoc.exists()) {
+          setUser(userDoc.data());
+        }
+      }
+    }
+
+    fetchUser();
+  }, [session]);
+
+  return (
+    <div className="flex justify-center h-screen">
+      {user && user.mbti ? (
+        <div className="grid m-auto text-center">
+          <div className="m-4">
+            {user.mbti + " " + user.name}님 환영합니다.
+          </div>
+          <button
+            className={`w-40
+                      justify-self-center
+                      p-1 mb-4
+                    bg-blue-500 text-white
+                      border border-blue-500 rounded
+                    hover:bg-white hover:text-blue-500`}
+            onClick={() => router.push("/")}
+          >
+            Go to Home
+          </button>
+          <button
+            className={`w-40
+                      justify-self-center
+                      p-1 mb-4
+                    text-blue-500
+                      border border-blue-500 rounded
+                    hover:bg-white hover:text-blue-500`}
+            onClick={() => signOut()}
+          >
+            Sign out
+          </button>
+        </div>
+      ) : (
+            <div className="grid m-auto text-center">
+              <div className="m-4">Not signed in</div>
+              <button
+                className={`w-40
+                          justify-self-center
+                          p-1 mb-4
+                        bg-blue-500 text-white
+                          border border-blue-500 rounded
+                        hover:bg-white hover:text-blue-500`}
+                onClick={() => signIn()}
+              >
+                Sign in
+              </button>
+            </div>
+      )}
+    </div>
+  );
+}
