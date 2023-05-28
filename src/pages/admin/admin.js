4ffd29ebf6217fc9f8f4b2412cee16e5ecbea482
hiddenjoy <hiddenjoy@snu.ac.kr>
2023-05-28 23:15:28 +0900

    smallanswer 컴포넌트 만들고 answer 더미데이터 만들어봄

--- src/pages/admin/admin.js
+++ src/pages/admin/admin.js
@@ -1,15 +1,15 @@
 import { useState, useEffect } from "react";
 import { collection, getDocs } from "firebase/firestore";
-import { db } from '@/firebase/index.js';
+import { db } from "@/firebase/index.js";
 
 export default function Admin() {
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
     async function fetchUsers() {
-      const usersCollection = collection(db, 'users');
+      const usersCollection = collection(db, "users");
       const userSnapshot = await getDocs(usersCollection);
-      const userList = userSnapshot.docs.map(doc => doc.data());
+      const userList = userSnapshot.docs.map((doc) => doc.data());
       setUsers(userList);
     }
 
@@ -20,12 +20,13 @@ export default function Admin() {
     <div className="flex justify-center h-screen">
       <div className="grid m-auto text-center">
         <div className="m-4">User List</div>
-        {users.map((user, index) => (
-          <div key={index}>
-            <p>Name: {user.name}</p>
-            <p>MBTI: {user.mbti}</p>
-          </div>
-        ))}
+        {users &&
+          users.map((user, index) => (
+            <div key={index}>
+              <p>Name: {user.name}</p>
+              <p>MBTI: {user.mbti}</p>
+            </div>
+          ))}
       </div>
     </div>
   );
