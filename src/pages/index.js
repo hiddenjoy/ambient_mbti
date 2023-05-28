3aef3e84ebec93b7d9ded79f43a8a0598ab1cf58
hiddenjoy <hiddenjoy@snu.ac.kr>
2023-05-28 10:02:52 +0900

    next auth 오류 임시방편

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,18 @@
 import Link from "next/link";
-
+// import { useRouter } from "next/router";
+// import { useSession } from "next-auth/react";
 import Main from "./main-page";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
-
 export default function Home() {
-  const router = useRouter();
-  const { data } = useSession({
-    required : true,
-    onUnauthenticated() {
-      router.replace("/auth/signin");
-    },
-  });
+  // const router = useRouter();
+  // const { data } = useSession({
+  //   required: true,
+  //   onUnauthenticated() {
+  //     router.replace("/auth/signin");
+  //   },
+  // });
 
   return (
     <div>
