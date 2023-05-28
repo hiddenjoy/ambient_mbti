31dd09966eb0d6d70ac7300a07dadf7546a48b12
shsha0110 <shsh0201@snu.ac.kr>
2023-05-28 16:53:30 +0900

    update: mbti 묻는 기능 추가
    mbti 묻는 기능 추가

--- src/pages/auth/signin.js
+++ src/pages/auth/signin.js
@@ -1,54 +1,82 @@
 import { useRouter } from "next/router";
 import { useSession, signIn, signOut } from "next-auth/react";
+import { useState } from "react";
+import { doc, updateDoc, getDoc, setDoc } from "firebase/firestore";
+import { db } from '@/firebase/index.js';
 
 export default function Signin() {
-    const router = useRouter();
-    const { data : session } = useSession();
+  const router = useRouter();
+  const { data: session } = useSession();
+  const [mbti, setMbti] = useState("");
 
-    return (
-        <div className="flex justify-center h-screen">
-          {session ? (
-            <div className="grid m-auto text-center">
-              <div className="m-4">Signed in as {session.user.name? session.user.name : session.user.email}</div>
-              <button
-                className={`w-40
-                          justify-self-center
-                          p-1 mb-4
-                        bg-blue-500 text-white
-                          border border-blue-500 rounded
-                        hover:bg-white hover:text-blue-500`}
-                onClick={ () => router.push("/")}
-              >
-                Go to Home
-              </button>
-              <button
-                className={`w-40
-                          justify-self-center
-                          p-1 mb-4
-                        text-blue-500
-                          border border-blue-500 rounded
-                        hover:bg-white hover:text-blue-500`}
-                onClick={() => signOut()}
-              >
-                Sign out
-              </button>
-            </div>
+  async function updateUserMbti(uid, mbti, name) {
+    const userRef = doc(db, 'users', uid);
+    const userSnapshot = await getDoc(userRef);
+  
+    if (userSnapshot.exists()) {
+      await updateDoc(userRef, { mbti, name });
+    } else {
+      await setDoc(userRef, { uid, mbti, name });
+    }
+
+    // Force session update after modifying the user document
+    signIn('credentials', { callbackUrl: '/auth/signedin' });
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (session) {
+      await updateUserMbti(session.user.id, mbti, session.user.name);
+    }
+  };
+
+  return (
+    <div className="flex justify-center h-screen">
+      {session ? (
+        <div className="grid m-auto text-center">
+          {!session.user.mbti ? (
+            <>
+              <div className="m-4">당신의 MBTI를 입력해주세요!</div>
+              <form onSubmit={handleSubmit}>
+                <input
+                  type="text"
+                  className="p-1 border border-gray-300"
+                  onChange={(e) => setMbti(e.target.value)}
+                  value={mbti}
+                />
+                <button 
+                  type="submit" 
+                  className={`w-40
+                    justify-self-center
+                    p-1 mt-4
+                    bg-blue-500 text-white
+                    border border-blue-500 rounded
+                    hover:bg-white hover:text-blue-500`}
+                >
+                  Submit
+                </button>
+              </form>
+            </>
           ) : (
-            <div className="grid m-auto text-center">
-              <div className="m-4">Not signed in</div>
-              <button
-                className={`w-40
-                          justify-self-center
-                          p-1 mb-4
-                        bg-blue-500 text-white
-                          border border-blue-500 rounded
-                        hover:bg-white hover:text-blue-500`}
-                onClick={async() => await signIn()}
-              >
-                Sign in
-              </button>
-            </div>
+            router.push("/auth/signedin")
           )}
         </div>
-      );
+      ) : (
+        <div className="grid m-auto text-center">
+          <div className="m-4">Not signed in</div>
+          <button
+            className={`w-40
+                      justify-self-center
+                      p-1 mb-4
+                    bg-blue-500 text-white
+                      border border-blue-500 rounded
+                    hover:bg-white hover:text-blue-500`}
+            onClick={() => signIn()}
+          >
+            Sign in
+          </button>
+        </div>
+      )}
+    </div>
+  );
 }
