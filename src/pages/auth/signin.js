614ca43588a7e0c54647e04364beaade677b99b2
jiyeeeah <sije3757@snu.ac.kr>
2023-05-29 22:06:48 +0900

    sign-in main copy/paste

--- src/pages/auth/signin.js
+++ src/pages/auth/signin.js
@@ -14,10 +14,19 @@ export default function Signin() {
   async function updateUserMbti(uid, mbti, name) {
     const userRef = doc(db, "users", uid);
     const userSnapshot = await getDoc(userRef);
+
     // Check if the user is an admin
     const isAdmin = admins.some(
       (admin) => admin.name === name && admin.mbti === mbti
     );
+
+    if (userSnapshot.exists() && userSnapshot.data().mbti !== mbti) {
+      // If user's existing mbti is different than the entered one,
+      // prompt for confirmation to change.
+      setConfirmChange(true);
+      return;
+    }
+
     if (userSnapshot.exists()) {
       await updateDoc(userRef, { mbti, name, isAdmin });
     } else {
@@ -46,6 +55,24 @@ export default function Signin() {
     setMbti(e.target.value.toUpperCase());
   };
 
+  const handleConfirmChange = async (change) => {
+    if (change) {
+      // Update MBTI if user confirms change
+      const userRef = doc(db, "users", session.user.id);
+      const isAdmin = admins.some(
+        (admin) => admin.name === session.user.name && admin.mbti === mbti
+      );
+      await updateDoc(userRef, { mbti, name: session.user.name, isAdmin });
+      // Force session update after modifying the user document
+      signIn("credentials", { callbackUrl: "/auth/signedin" });
+    } else {
+      // Reset MBTI input if user denies change
+      setMbti("");
+    }
+    // Reset confirmChange state
+    setConfirmChange(false);
+  };
+
   return (
     <div className="flex justify-center h-screen">
       {session ? (
@@ -54,54 +81,12 @@ export default function Signin() {
             <>
               <div className="m-4">당신의 MBTI를 입력해주세요!</div>
               <form onSubmit={handleSubmit}>
-                {/* <input
+                <input
                   type="text"
                   className="p-1 border border-gray-300"
                   onChange={handleInputChange}
                   value={mbti}
-                /> */}
-                <select
-                  id="mbti"
-                  name={mbti}
-                  onChange={handleInputChange}
-                  value={mbti}
-                />{" "}
-                */}
-                <select
-                  id="mbti"
-                  name={mbti}
-                  onChange={(e) => setMbti(e.target.value)}
-                  value={mbti}
-                />{" "}
-                */}
-                <select
-                  id="mbti"
-                  name={mbti}
-                  onChange={handleInputChange}
-                  value={mbti}
-                />{" "}
-                <select
-                  id="mbti"
-                  name={mbti}
-                  onChange={(e) => setMbti(e.target.value)}
-                >
-                  <option value="ISTJ">ISTJ</option>
-                  <option value="ISFJ">ISFJ</option>
-                  <option value="INTJ">INTJ</option>
-                  <option value="INFJ">INFJ</option>
-                  <option value="ISTP">ISTP</option>
-                  <option value="ISFP">ISFP</option>
-                  <option value="INTP">INTP</option>
-                  <option value="INFP">INFP</option>
-                  <option value="ESTP">ESTP</option>
-                  <option value="ESFP">ESFP</option>
-                  <option value="ENTP">ENTP</option>
-                  <option value="ENFP">ENFP</option>
-                  <option value="ENTP">ESTJ</option>
-                  <option value="ENFP">ESFJ</option>
-                  <option value="ENTP">ENTJ</option>
-                  <option value="ENFP">ENFJ</option>
-                </select>
+                />
                 <button
                   type="submit"
                   className={`w-40
