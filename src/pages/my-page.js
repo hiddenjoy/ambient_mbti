600516633cfbd87bbceb276462e3591fc80a2f0e
taye826 <tayejang@gmail.com>
2023-05-29 21:43:26 +0900

    답변 로직과 캘린더 추가

--- src/pages/my-page.js
+++ src/pages/my-page.js
@@ -39,10 +39,12 @@ const WeeklyCalendar = () => {
 const Mypage = () => {
   const { data: session } = useSession();
   const [user, setUser] = useState(null);
+  const [questionAnswers, setQuestionAnswers] = useState([]);
+  const [likedAnswers, setLikedAnswers] = useState([]);
 
   useEffect(() => {
     async function fetchUser() {
-      if (session) {
+      if (session && session.user) {
         const userRef = doc(db, "users", session.user.id);
         const userDoc = await getDoc(userRef);
 
@@ -70,12 +72,42 @@ const Mypage = () => {
             <h2 className="text-2xl font-bold">{question.question}</h2>
             <p className="text-gray-500">{question.askDate}</p>
             {/* 답변을 보여주는 로직을 구현해주세요 */}
+            {questionAnswers && questionAnswers.map((qa) => {
+              if (qa.questionId === question.id) {
+                return (
+                  <div key={qa.id} className="mb-4">
+                    <p className="text-lg font-semibold mb-2">{qa.askDate}</p>
+                    <p className="text-lg">질문: {qa.question}</p>
+                    <p className="text-lg">답변: {qa.answer}</p>
+                  </div>
+                );
+              }
+              return null;
+            })}
           </div>
         ))}
 
         <div className="my-8">
+          <h2 className="text-3xl font-bold mb-4">날짜별 질문과 답변</h2>
+          {/* 날짜별 질문과 답변을 렌더링하는 로직 */}
+          {questionAnswers && questionAnswers.map((qa) => (
+            <div key={qa.id} className="mb-4">
+              <p className="text-lg font-semibold mb-2">{qa.askDate}</p>
+              <p className="text-lg">질문: {qa.question}</p>
+              <p className="text-lg">답변: {qa.answer}</p>
+            </div>
+          ))}
+        </div>
+
+        <div className="my-8">
           <h2 className="text-3xl font-bold mb-4">좋아요를 표시한 답변</h2>
-          {/* 여기다 좋아요 답변 로직 추가할 예정입니다! */}
+          {likedAnswers.map((answer) => (
+            <div key={answer.id} className="mb-4">
+              <p className="text-lg font-semibold mb-2">{answer.askDate}</p>
+              <p className="text-lg">질문: {answer.question}</p>
+              <p className="text-lg">답변: {answer.answer}</p>
+            </div>
+          ))}
         </div>
       </main>
     </Layout>
