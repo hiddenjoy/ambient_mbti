1619dcb3e5bc0452dc62c3f94edecacf954fe327
jiyeeeah <sije3757@snu.ac.kr>
2023-06-02 21:23:23 +0900

    답변 보는 요소 수정 (좋아요 버튼)

--- src/components/SmallAnswerList.js
+++ src/components/SmallAnswerList.js
@@ -1,9 +1,19 @@
+import { useState } from "react";
+
 const SmallAnswerList = ({ key, answer }) => {
+  const [liked, setLiked] = useState(false);
+
+  const toggleLike = () => {
+    setLiked(!liked);
+  };
+
   return (
-    <div className="my-2 bg-lime-100 p-3 w-full rounded">
-      <div>답변: {answer.content}</div>
-      <div>답변자 mbti: {answer.user.mbti}</div>
-      <div>❤️{answer.likeUsers.length}</div>
+    <div className="my-2 bg-lime-100 p-3 w-full rounded relative flex flex-row items-center justify-between">
+      <div className="flex flex-row items-center">
+        <div className="border mx-3">{answer.user.mbti}</div>
+        <div>답변: {answer.content}</div>
+      </div>
+      <button onClick={toggleLike}>{liked ? "❤️" : "🤍"}  {answer.likeUsers.length}</button>
     </div>
   );
 };
