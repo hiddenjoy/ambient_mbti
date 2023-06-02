ab7945bbe5ad2f7f71cfc58a84086be60c40c4f1
hiddenjoy <hiddenjoy@snu.ac.kr>
2023-06-02 16:28:51 +0900

    메인 페이지 question에 수정 버튼 추가 (아직 수정 기능은 추가 안 함)

--- src/pages/index.js
+++ src/pages/index.js
@@ -105,7 +105,16 @@ export default function Home() {
   return (
     <>
       <div className="flex flex-col h-screen justify-between">
-        <Layout>{isLoggedIn && !isAnsweredToday ? <Today /> : <Main />}</Layout>
+        <Layout>
+          {isLoggedIn && !isAnsweredToday ? (
+            <Today />
+          ) : (
+            <Main
+              isAnsweredToday={isAnsweredToday}
+              setIsAnsweredToday={setIsAnsweredToday}
+            />
+          )}
+        </Layout>
         <Footer />
       </div>
     </>
--- src/pages/main-page.js
+++ src/pages/main-page.js
@@ -9,7 +9,7 @@ import AnswerList from "../components/AnswerList";
 import TempAnswerList from "@/components/tempAnswerList";
 import Question from "@/components/Question";
 
-const Main = () => {
+const Main = ({ isAnsweredToday, setIsAnsweredToday }) => {
   const { data: session } = useSession();
   const [user, setUser] = useState(null);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -35,40 +35,13 @@ const Main = () => {
     <>
       {/* 랜덤한 답변 보여주기 */}
       {isLoggedIn ? (
-        // 이전의 코드
-        // (
-        //   <>
-        //     <div className="bg-white w-full p-5 text-black">
-        //       <h1>{user?.mbti} 친구들의 답변 모아보기</h1>
-        //       {/* 답변들 div */}
-        //       <div className="mt-3 px-20 flex items-center justify-between">
-        //         {/* content 이 부분은 한번에렌더링 할거긴 함 */}
-        //         <AnswerList answerList={answerList} />
-        //       </div>
-        //     </div>
-        //     {/* mbti별 인기 답변 */}
-        //     <div className=" bg-neutral-200 w-full p-5 text-black">
-        //       MBTI별 인기 답변
-        //       <div className="mt-3 grid grid-cols-4 gap-4">
-        //         {answer.map((item) => (
-        //           <SmallAnswer key={item.id} answer={item} />
-        //         ))}
-        //       </div>
-        //     </div>
-        //     <Link
-        //       href="/compare"
-        //       className="inline-block text-2xl font-bold p-5 text-center bg-gray-400 text-white rounded-full mt-5"
-        //     >
-        //       MBTI 별로 비교
-        //     </Link>
-        //   </>
-        // )
         <>
           <div className="flex flex-row">
-            <div className="basis-1/3 flex flex-col items-center">
-              <div className="bg-neutral-100 basis-1/2">
-                <Question />
-              </div>
+            <div className="basis-1/3 flex flex-col bg-neutral-100 items-center">
+              <Question
+                isAnsweredToday={isAnsweredToday}
+                setIsAnsweredToday={setIsAnsweredToday}
+              />
             </div>
             <div className="basis-2/3 px-10 w-full">
               {/* content 이 부분은 한번에 렌더링 할거긴 함 */}
