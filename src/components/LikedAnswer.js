21bba176ad73890619568212b72bab11e98aaca5
ponniepark <127806879+ponniepark@users.noreply.github.com>
2023-06-03 22:26:29 +0900

    likedanswer에서 불러오는 것 변경하기

--- src/components/LikedAnswer.js
+++ src/components/LikedAnswer.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import TempAnswerList from "@/components/tempAnswerList";
+import AnswerList from "../components/AnswerList";
+import mbti from "@/data/mbti";
 
 const LikedAnswers = () => {
   const [likedAnswers, setLikedAnswers] = useState([]);
@@ -9,7 +11,9 @@ const LikedAnswers = () => {
       <h2 className="text-3xl font-bold mb-4 "  >좋아요를 표시한 답변</h2>
       <div className="w-full">
         <div className="px-50">
-          <TempAnswerList num={6} />
+        <AnswerList
+                mbti={mbti}
+              />
         </div>
       </div>
     </div>
