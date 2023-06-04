6111c644280b4a028f50ff9f6b3f392e0f1fc57f
jiyeeeah <sije3757@snu.ac.kr>
2023-06-04 21:54:33 +0900

    좋아요 기능 추가

--- src/components/SmallAnswerList.js
+++ src/components/SmallAnswerList.js
@@ -1,10 +1,42 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { db } from "@/firebase";
+import { collection, doc, getDoc, updateDoc } from "firebase/firestore";
 
-const SmallAnswerList = ({ key, answer }) => {
-  const [liked, setLiked] = useState(false);
+const userCollection = collection(db, "users");
+const answerCollection = collection(db, "answers");
 
-  const toggleLike = () => {
-    setLiked(!liked);
+const SmallAnswerList = ({ answer, onClick }) => {
+  const { data } = useSession();
+  const [liked, setLiked] = useState();
+  const [likedUserNum, setLikedUserNum] = useState();
+
+  useEffect(() => {
+    setLikedUserNum(answer.likeUsers.length);
+    console.log(likedUserNum);
+  }, [answer.likeUsers]);
+
+  const likeAnswer = async (answerId) => {
+    const answerRef = doc(answerCollection, answerId);
+    const answerSnapShot = await getDoc(answerRef);
+    const answerData = answerSnapShot.data();
+    const likedAnswerData = Boolean(answerData.likeUsers.length > 0 ? answerData.likeUsers.find((i) => i === data.user.id) : false);
+
+    if(likedAnswerData) {
+      const updatedLikeUsers = answerData.likeUsers.filter(
+        (userId) => userId !== data.user.id
+      );
+      await updateDoc(answerRef, { likeUsers: updatedLikeUsers });
+      setLiked(false);
+      setLikedUserNum(updatedLikeUsers.length);
+      console.log(likedUserNum);
+    } else {
+      const updatedLikeUsers = [...answerData.likeUsers, data.user.id];
+      await updateDoc(answerRef, { likeUsers: updatedLikeUsers });
+      setLiked(true);
+      setLikedUserNum(updatedLikeUsers.length);
+      console.log(likedUserNum);
+    }
   };
 
   return (
@@ -16,7 +48,7 @@ const SmallAnswerList = ({ key, answer }) => {
       <div className="text-end text-xs italic">
         by. {answer.user.mbti} {answer.user.id}
       </div>
-      <button onClick={toggleLike}>{liked ? "❤️" : "🤍"}  {answer.likeUsers.length}</button>
+      <button onClick={() => likeAnswer(answer.id)}>{liked ? "❤️" : "🤍"}  {likedUserNum}</button>
 
     </div>
   );
