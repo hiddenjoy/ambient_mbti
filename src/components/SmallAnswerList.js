7448ceecae97d0caa04952043d7322ef2a17673f
hiddenjoy <hiddenjoy@snu.ac.kr>
2023-06-08 13:53:18 +0900

    mbti별 색상 적용

--- src/components/SmallAnswerList.js
+++ src/components/SmallAnswerList.js
@@ -2,11 +2,13 @@ import { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
 import { db } from "@/firebase";
 import { collection, doc, getDoc, updateDoc } from "firebase/firestore";
+import mbtiColors from "../data/mbtiColors.js";
 
 const userCollection = collection(db, "users");
 const answerCollection = collection(db, "answers");
 
 const SmallAnswerList = ({ answer }) => {
+  const [bgColor, setBgColor] = useState("#E5E7EB"); // 기본 배경색 설정
   const { data } = useSession();
   const [liked, setLiked] = useState();
   const [likedUserNum, setLikedUserNum] = useState();
@@ -23,7 +25,6 @@ const SmallAnswerList = ({ answer }) => {
   }, [answer.likeUsers]);
 
   const likeAnswer = async (answerId) => {
-    console.log(answerId);
     const answerRef = doc(answerCollection, answerId);
     const answerSnapShot = await getDoc(answerRef);
     const answerData = answerSnapShot.data();
@@ -56,8 +57,22 @@ const SmallAnswerList = ({ answer }) => {
 
   const truncatedContent = truncateText(answer.content, 50); // 글자 제한을 50으로 설정
 
+  useEffect(() => {
+    if (answer.user.mbti) {
+      console.log(mbtiColors);
+      const mbtiColor = mbtiColors[answer.user.mbti]; // mbti.js에서 해당 mbti의 색상을 가져옴
+
+      if (mbtiColor) {
+        setBgColor(mbtiColor);
+      }
+    }
+  }, []);
+
   return (
-    <div className="flex flex-col justify-between my-2 bg-primary text-tertiary p-3 w-full rounded">
+    <div
+      className="flex flex-col justify-between my-2 text-neutral-800 p-3 w-full rounded"
+      style={{ backgroundColor: bgColor }} // 배경색을 동적으로 설정
+    >
       <div className=" border text-lg text-center mb-3">
         " {truncatedContent} "
       </div>
