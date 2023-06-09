9c14b90a6310a88d89890e7ee76ce63da82762e4
ponniepark <127806879+ponniepark@users.noreply.github.com>
2023-06-10 04:19:36 +0900

    좋아요한 답변한 글이 없거나, 팔로우한 유저가 없을 때 관련 이미지가 뜨도록 구현

--- src/components/FollowingUser.js
+++ src/components/FollowingUser.js
@@ -55,7 +55,64 @@ const FollowingUsers = () => {
           </Link>
         ))
       ) : (
-        <div>팔로워가 없습니다 ㅠㅠ</div>
+        // <div>팔로워가 없습니다 ㅠㅠ</div>
+
+        <div className>
+        <div className="flex flex-row items-center h-full">
+          <div className="basis-1/3 flex h-full flex-col items-end"></div>
+          <div className="basis-1/2 flex h-full flex-col items-center ">
+            <div className=" w-full ">
+              <div className="w-full mt-10 mb-10 items-center">
+                <img
+                  src="/images/Ambers follow.png"
+                  alt="0 팔로잉 이미지"
+                  style={{
+                    maxWidth: "40%",
+                    height: "auto",
+                    maxHeight: "auto",
+                    display: "block",
+                    top: "50%",
+                    transform: "translateY(10%)",
+                    marginTop: "20% auto",
+                    marginLeft: "10%",
+                    marginTop: "25%",
+                  }}
+                />
+                <div
+                  className="basis-1/2 mb-5 w-full text-center mt-5  "
+                  style={{
+                    marginLeft: "-19%",
+                    marginTop: "6%",
+                    display: "block",
+                    fontWeight: "bold",
+                    fontSize: "20px",
+                    color: "#",
+                    whiteSpace: "nowrap",
+                  }}
+                >
+                  팔로우한 유저가 없어요!
+                </div>
+
+                {/* <div
+                  className="basis-1/2 mb-1 w-full text-center mt-1 textAlign:'center'"
+                  style={{
+                    marginLeft: "-17%",
+                    display: "block",
+                    fontWeight: "regular",
+                    marginRight: "-17%",
+                    fontSize: "14px",
+                    color: "#6D6E71",
+                    whiteSpace: "nowrap",
+                  }}
+                >
+                  로그인하여 당신의 이야기를 들려주세요!
+                </div> */}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
       )}
       </div>
   );
---
+++ src/components/LikedAnswerClick.js
@@ -0,0 +1,117 @@
+import React, { useState, useEffect } from "react";
+import SmallAnswerList from "@/components/AmbientAnswerList";
+import { useSession, signOut } from "next-auth/react";
+
+import { db } from "@/firebase";
+import {
+  collection,
+  query,
+  doc,
+  getDoc,
+  getDocs,
+  addDoc,
+  updateDoc,
+  deleteDoc,
+  orderBy,
+  where,
+} from "firebase/firestore";
+
+const answerCollection = collection(db, "answers");
+const userCollection = collection(db, "users");
+
+const AnswerList = ({ mbti, date }) => {
+  const [answers, setAnswers] = useState([]);
+  const { data } = useSession();
+  const [layout, setLayout] = useState(true);
+  const [noMbtiList, setNoMbtiList] = useState([]);
+
+  const getAnswers = async () => {
+    const q = query(
+      answerCollection,
+      where("user.mbti", "==", mbti),
+      where("questionDate", "==", date)
+    );
+
+    const results = await getDocs(q);
+
+    const newAnswers = [];
+    results.docs.forEach((doc) => {
+      newAnswers.push({ id: doc.id, ...doc.data() });
+    });
+
+    setAnswers(newAnswers);
+  };
+
+  useEffect(() => {
+    getAnswers();
+  }, [mbti, date]);
+
+  const handleRandom = () => {
+    const shuffledAnswers = [...answers];
+    for (let i = shuffledAnswers.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledAnswers[i], shuffledAnswers[j]] = [
+        shuffledAnswers[j],
+        shuffledAnswers[i],
+      ];
+    }
+    setAnswers(shuffledAnswers);
+  };
+
+  const toList = () => {
+    setLayout(true);
+  };
+  const toGallery = () => {
+    setLayout(false);
+  };
+
+  const handleSort = () => {
+    const sortedAnswers = [...answers];
+    sortedAnswers.sort((a, b) => b.likeUsers.length - a.likeUsers.length);
+    setAnswers(sortedAnswers);
+  };
+
+  return (
+    <div className="flex flex-col w-full">
+      <div className="sticky top-0 border w-full flex justify-between bg-white">
+        <div>
+          <button className="border my-1 ml-1 p-1" onClick={toList}>
+            list
+          </button>
+          <button className="border my-1 mr-1 p-1" onClick={toGallery}>
+            gallery
+          </button>
+        </div>
+        <div>
+          <button className="border my-1 mr-1 p-1 " onClick={handleSort}>
+            좋아요 순
+          </button>
+          <button className="border my-1 mr-1 p-1" onClick={handleRandom}>
+            ↺
+          </button>
+        </div>
+      </div>
+      <div className="overflow-y-auto w-full h-[450px]">
+        {layout ? (
+          <div
+            className={`flex flex-col items-center w-full ${
+              answers.length > 2 ? "animate-slide-up" : ""
+            }`}
+          >
+            {answers.map((item) => (
+              <SmallAnswerList key={item.id} answer={item} />
+            ))}
+          </div>
+        ) : (
+          <div className="grid grid-cols-3 gap-2">
+            {answers.map((item) => (
+              <SmallAnswerList key={item.id} answer={item} />
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default AnswerList;
