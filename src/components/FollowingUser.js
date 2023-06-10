6e3049f58658932019dcd740b6a7cdcee8167e6c
ponniepark <127806879+ponniepark@users.noreply.github.com>
2023-06-10 21:55:05 +0900

    9시 54분 이미지 위치 수정하려고 함

--- src/components/FollowingUser.js
+++ src/components/FollowingUser.js
@@ -60,62 +60,41 @@ const FollowingUsers = () => {
         ))
       ) : (
         // <div>팔로워가 없습니다 ㅠㅠ</div>
-
-        <div className>
-        <div className="flex flex-row items-center h-full">
-          <div className="basis-1/3 flex h-full flex-col items-end"></div>
-          <div className="basis-1/2 flex h-full flex-col items-center ">
-            <div className=" w-full ">
-              <div className="w-full mt-10 mb-10 items-center">
-                <img
-                  src="/images/Ambers follow.png"
-                  alt="0 팔로잉 이미지"
-                  style={{
-                    maxWidth: "40%",
-                    height: "auto",
-                    maxHeight: "auto",
-                    display: "block",
-                    top: "50%",
-                    transform: "translateY(10%)",
-                    marginTop: "20% auto",
-                    marginLeft: "10%",
-                    marginTop: "25%",
-                  }}
-                />
-                <div
-                  className="basis-1/2 mb-5 w-full text-center mt-5  "
-                  style={{
-                    marginLeft: "-19%",
-                    marginTop: "6%",
-                    display: "block",
-                    fontWeight: "bold",
-                    fontSize: "20px",
-                    color: "#",
-                    whiteSpace: "nowrap",
-                  }}
-                >
-                  팔로우한 유저가 없어요!
-                </div>
-
-                {/* <div
-                  className="basis-1/2 mb-1 w-full text-center mt-1 textAlign:'center'"
-                  style={{
-                    marginLeft: "-17%",
-                    display: "block",
-                    fontWeight: "regular",
-                    marginRight: "-17%",
-                    fontSize: "14px",
-                    color: "#6D6E71",
-                    whiteSpace: "nowrap",
-                  }}
-                >
-                  로그인하여 당신의 이야기를 들려주세요!
-                </div> */}
-              </div>
-            </div>
-          </div>
-        </div>
+<div className="flex flex-row items-center h-full">
+  <div className="flex-grow items-center"></div>
+  <div className="basis-4/5 flex h-full flex-col items-center justify-center text-center" style={{ maxWidth: "80%", minWidth : "70%" }}>
+    <div className="w-full mt-10 mb-10 flex flex-col items-center">
+      <div className="flex justify-center items-center border">
+        <img
+          src="/images/Ambers follow.png"
+          alt="0 팔로잉 이미지"
+          style={{
+            maxWidth: "40%",
+            height: "auto",
+            maxHeight: "auto",
+            display: "block",
+            margin: "0 auto",
+          }}
+        />
       </div>
+      <div
+        className="mb-5 w-full text-center mt-5"
+        style={{
+          fontWeight: "bold",
+          fontSize: "20px",
+          color: "#",
+          whiteSpace: "nowrap",
+          textAlign: "center",
+        }}
+      >
+        팔로우한 유저가 없어요!
+      </div>
+    </div>
+  </div>
+  <div className="flex-grow"></div>
+</div>
+
+
 
       )}
       </div>
