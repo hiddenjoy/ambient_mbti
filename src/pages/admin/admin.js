f5c8078f7cf11a54c6987164d214bee4a2b5c1da
shsha0110 <shsh0201@snu.ac.kr>
2023-06-11 18:10:22 +0900

    create: admin/question.js
    날짜별 질문 확인 및 수정, 추가할 수 있는 기능 구현

--- src/pages/admin/admin.js
+++ src/pages/admin/admin.js
@@ -2,11 +2,13 @@ import { useState, useEffect } from "react";
 import { collection, getDocs, query, orderBy } from "firebase/firestore";
 import { db } from "@/firebase/index.js";
 import UserProfileForAdminPage from "@/components/UserProfileForAdminPage";
+import { useRouter } from "next/router";
 
 export default function Admin() {
+  const router = useRouter();
   const [users, setUsers] = useState([]);
   const [page, setPage] = useState(1);
-  const [filter, setFilter] = useState("");
+  const [filter, setFilter] = useState("전체");
 
   useEffect(() => {
     async function fetchUsers() {
@@ -34,11 +36,30 @@ export default function Admin() {
     if (filter === "기타") {
       return user.name && !/^[ㄱ-ㅎ]/.test(getChosung(user.name[0]));
     }
+    if (filter === "전체") {
+      return true;
+    }
     return user.name && getChosung(user.name[0]) === filter;
   });
 
   return (
     <div className="flex justify-center h-screen p-4 m-8">
+      <div className="w-1/12 p-4 ml-4">
+        <button
+          onClick={() => router.push("/admin/admin")}
+          className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4 block text-left text-xl"
+          style={{width: '200px'}}
+        >
+          Users
+        </button>
+        <button
+          onClick={() => router.push("/admin/question")}
+          className="w-full bg-white hover:bg-gray-200 text-black font-bold py-2 px-4 rounded mb-4 block text-left text-xl"
+          style={{width: '200px'}}
+        >
+          Questions
+        </button>
+      </div>
       <div className="w-3/4 bg-white rounded-lg shadow p-4 ml-4">
         <div className="flex justify-between mb-4">
           <button 
@@ -61,12 +82,13 @@ export default function Admin() {
           <UserProfileForAdminPage key={index} user={user} />
         ))}
       </div>
-      <div className="w-1/12 bg-white rounded-lg shadow p-4 ml-4">
-        {['ㄱ','ㄴ','ㄷ','ㄹ','ㅁ','ㅂ','ㅅ','ㅇ','ㅈ','ㅊ','ㅋ','ㅌ','ㅍ','ㅎ','기타'].map((letter, index) => (
+      <div className="w-1/12 p-4 ml-4">
+        {['ㄱ','ㄴ','ㄷ','ㄹ','ㅁ','ㅂ','ㅅ','ㅇ','ㅈ','ㅊ','ㅋ','ㅌ','ㅍ','ㅎ','기타', '전체'].map((letter, index) => (
           <button 
             key={index}
-            className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4"
+            className={`w-full font-bold py-2 px-4 rounded mb-4 ${letter === filter ? 'bg-blue-500 text-white' : 'bg-white text-black hover:bg-gray-200'}`}
             onClick={() => { setPage(1); setFilter(letter); }}
+            style={{width: '140px'}}
           >
             {letter}
           </button>
---
+++ src/pages/admin/question.js
@@ -0,0 +1,155 @@
+// pages/admin/question.js
+import { useState, useEffect } from "react";
+import { collection, getDocs, addDoc, serverTimestamp, query, orderBy } from "firebase/firestore";
+import { db } from "@/firebase/index.js";
+import { useRouter } from "next/router";
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import styles from '@/styles/Calendar.module.css';
+import QuestionForAdminPage from "@/components/QuestionForAdminPage";
+
+export default function AdminQuestion() {
+  const router = useRouter();
+  const [questions, setQuestions] = useState([]);
+  const [selectedDate, setSelectedDate] = useState(null);
+  const [currentQuestion, setCurrentQuestion] = useState(null);
+
+  useEffect(() => {
+    async function fetchQuestions() {
+      const questionsCollection = collection(db, "questions");
+      const questionSnapshot = await getDocs(query(questionsCollection, orderBy("date", "desc")));
+      const questionsArray = questionSnapshot.docs.reduce((accumulator, doc) => {
+        const data = doc.data();
+        try {
+          const date = new Date(data.date.seconds * 1000); // Convert Firestore timestamp to JavaScript Date
+          const formattedDate = date.toISOString().split("T")[0]; // Convert to yyyy-mm-dd format
+          accumulator.push({
+            id: doc.id,
+            date: formattedDate,
+            content: data.content,
+          });
+        } catch (e) {
+          console.error(`Invalid date in document ${doc.id}: `, e);
+        }
+        return accumulator;
+      }, []);
+      setQuestions(questionsArray);
+    }
+
+    fetchQuestions();
+  }, []);
+
+  useEffect(() => {
+    if (selectedDate) {
+      const foundQuestion = questions.find(question => question.date === selectedDate);
+      if (foundQuestion) {
+        setCurrentQuestion(foundQuestion);
+      } else {
+        setCurrentQuestion(null);
+      }
+    }
+  }, [selectedDate, questions]);
+
+  async function handleAddQuestion() {
+    try {
+      const docRef = await addDoc(collection(db, "questions"), {
+        date: new Date(selectedDate),
+        content: "질문을 입력하세요."
+      });
+      const newQuestion = {
+        id: docRef.id,
+        date: selectedDate,
+        content: "질문을 입력하세요."
+      };
+      setQuestions(prevQuestions => [...prevQuestions, newQuestion]);
+      setCurrentQuestion(newQuestion);
+    } catch (error) {
+      console.error("Error adding question: ", error);
+    }
+  }
+
+  function handleQuestionUpdate(questionDate, updatedContent) {
+    setQuestions((prevQuestions) => {
+      return prevQuestions.map((question) => {
+        if (question.date === questionDate) {
+          return { ...question, content: updatedContent };
+        }
+        return question;
+      });
+    });
+  }
+
+  return (
+    <div className="flex justify-center h-screen p-4 m-8">
+      <div className="w-1/12 p-4 ml-4">
+        <button
+          onClick={() => router.push("/admin/admin")}
+          className="w-full bg-white hover:bg-gray-200 text-black font-bold py-2 px-4 rounded mb-4 block text-left text-xl"
+          style={{width: '200px'}}
+        >
+          Users
+        </button>
+        <button
+          onClick={() => router.push("/admin/question")}
+          className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4 block text-left text-xl"
+          style={{width: '200px'}}
+        >
+          Questions
+        </button>
+      </div>
+      <div className="w-3/4 bg-white rounded-lg shadow p-4 ml-4">
+        <div className="flex">
+          <div className={`${styles.reactCalendar} w-1/2 p-4`}>
+            <h2 className="text-2xl mb-4">날짜</h2>
+            <div className={`${styles.calendarWrapper}`}>
+                <Calendar
+                    locale="ko-KR"
+                    value={selectedDate}
+                    onClickDay={date => {
+                        const localDate = new Date(date.getTime() - date.getTimezoneOffset() * 60000);
+                        setSelectedDate(localDate.toISOString().split('T')[0]);
+                    }}
+                    tileClassName={({ date, view }) => {
+                        const localDate = new Date(date.getTime() - date.getTimezoneOffset() * 60000);
+                        const dateString = localDate.toISOString().split('T')[0];
+                        const questionExists = !!questions.find((q) => q.date === dateString); 
+                        return questionExists ? styles.calendarDayQuestionExists : styles.calendarDayQuestionNotExists;
+                    }}
+                    />
+            </div>
+          </div>
+          <div className="w-1/2 p-4">
+            <h2 className="text-2xl mb-4">질문</h2>
+            {currentQuestion ? (
+              <QuestionForAdminPage
+                key={currentQuestion.id}
+                question={currentQuestion}
+                onQuestionUpdate={handleQuestionUpdate}
+              />
+            ) : (
+              selectedDate && (
+                <div>
+                  <p>질문을 추가하시겠습니까?</p>
+                  <div className="flex">
+                    <button
+                      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4 mr-2"
+                      onClick={handleAddQuestion}
+                    >
+                      Yes
+                    </button>
+                    <button
+                      className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mb-4"
+                      onClick={() => setSelectedDate(null)}
+                    >
+                      No
+                    </button>
+                  </div>
+                </div>
+              )
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
